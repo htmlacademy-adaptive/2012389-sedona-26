0d104e818fa5e16a6f6728ca68fca163d9b7027a
Daria Mey <d.like.jagger@gmail.com>
2022-08-24 22:11:17 +0300

    Декоративные элементы

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,6 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let logo = document.querySelector('.logo')
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -7,8 +8,11 @@ navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    logo.classList.add('logo--none');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    logo.classList.remove('logo--none');
   }
 });
+
